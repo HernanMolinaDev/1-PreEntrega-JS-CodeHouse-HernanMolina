672ed18e6efac598d035bce554e8e28ac85c315d
HernanMolinaDev <hernanjmolinadev@gmail.com>
2023-08-10 18:42:00 -0200

    preentrega1-js

---
+++ javascript.js
@@ -0,0 +1,34 @@
+
+let monto;
+let meses;
+for(let i=true;i==true;){
+
+monto=prompt("Vamos a simular tu prestamo, a continuacion indica cuanto deseas solicitar por favor;");
+meses=prompt("coloque la cantidad de cuotas que desea abonar");
+    if (monto<50000 && meses<6){
+    Cuotas= monto*1.8/meses;
+    alert("tu prestamo por  "+monto+" a " +meses+" meses es de "+Cuotas+" cada mes para finarlizar el pago");
+    }
+    else if(monto<50000 && meses>=6 || monto>50000 && meses<6){
+        Cuotas= monto*2.2/meses;
+        alert("tu prestamo por  "+monto+" a " +meses+" meses es de "+Cuotas+" cada mes para finarlizar el pago");
+        }
+    else if(monto>50000 && meses<6){
+        Cuotas= monto*3.5/meses;
+        alert("tu prestamo por  "+monto+" a " +meses+" meses es de "+Cuotas+" cada mes para finarlizar el pago");
+        }
+        else{
+            alert("ingrese por favor datos validos (numeros positivos,caracteres validos ,etc)");
+        };
+    respuesta=prompt("Desea realizar otra simulacion? coloque SI o NO↓");
+    if(respuesta==="SI"){
+        alert("continuamos con las simulaciones!");
+    } else if(respuesta==="NO"){
+        alert("Gracias por utilizar nuestro servicio!");
+        i=false;
+        break
+    }
+    else{
+        alert("Por favor coloque una opcion valida")
+    }
+}
